6178f28897868c30d2bcccca57e7b995d0c1994d
LICF01 <lucascubilla@gmail.com>
2022-02-09 00:55:00 -0300

    Added repo creation date to the repo item

--- src/components/user/RepoItem.jsx
+++ src/components/user/RepoItem.jsx
@@ -7,21 +7,35 @@ import {
 	Box,
 	Badge,
 	Flex,
+	Spacer,
 	Heading,
 	Text,
 	Icon,
 } from '@chakra-ui/react';
 
-import { FiStar } from 'react-icons/fi';
+import { FiStar, FiClock } from 'react-icons/fi';
 import { BiGitRepoForked } from 'react-icons/bi';
 
 const RepoItem = ({ repo }) => {
-	const { name, description, priv, language, stargazers_count, forks_count } =
-		repo;
+	const {
+		name,
+		description,
+		priv,
+		language,
+		stargazers_count,
+		forks_count,
+		created_at,
+	} = repo;
+
+	const date = new Date(created_at).toLocaleString('default', {
+		month: 'short',
+		day: 'numeric',
+		year: 'numeric',
+	});
 
 	return (
 		<Box
-			borderWidth='1px'
+			bgColor='white'
 			borderRadius='lg'
 			overflow='hidden'
 			p='6'
@@ -32,18 +46,25 @@ const RepoItem = ({ repo }) => {
 			}}
 			sx={{ cursor: 'pointer' }}
 		>
+			<Flex justifyContent='end' alignItems='center' color='gray.400' mb='4'>
+				<Text borderRadius='full' >
+					{priv ? 'Private' : 'Public'}
+				</Text>
+				<Spacer />
+				<HStack>
+					<Icon as={FiClock} />
+					<Text>{date}</Text>
+				</HStack>
+			</Flex>
 			<Flex justifyContent='space-Between' alignItems='center'>
 				<Heading size='md'>{name}</Heading>
-				<Badge borderRadius='full' px='2' py='1' colorScheme='teal'>
-					{priv ? 'Private' : 'Public'}
-				</Badge>
 			</Flex>
 			<Text color='gray.500' mt={3}>
 				{description}
 			</Text>
 			<HStack mt={5}>
 				{language && (
-					<Badge borderRadius='full' px='4' py='1'>
+					<Badge borderRadius='full' px='4' py='1' color='gray.500'>
 						{language}
 					</Badge>
 				)}
