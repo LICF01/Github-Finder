0bbfac0348065e12101174b2d91b981e7c746370
LICF01 <lucascubilla@gmail.com>
2022-02-16 18:57:15 -0300

    Added with 100 for searh bar

--- src/components/user/SearchUser.jsx
+++ src/components/user/SearchUser.jsx
@@ -1,58 +1,58 @@
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { searchUsers } from '../../context/GithubActions';
-import GithubContext from '../../context/GithubContext';
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { searchUsers } from "../../context/GithubActions";
+import GithubContext from "../../context/GithubContext";
 
 import {
-	FormControl,
-	Input,
-	InputGroup,
-	InputLeftElement,
-} from '@chakra-ui/react';
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 
-import { RiSearchLine } from 'react-icons/ri';
+import { RiSearchLine } from "react-icons/ri";
 
 const SearchUser = () => {
-	const [inputText, setInputText] = useState('');
-	const { dispatch } = useContext(GithubContext);
-	let navigate = useNavigate();
+  const [inputText, setInputText] = useState("");
+  const { dispatch } = useContext(GithubContext);
+  let navigate = useNavigate();
 
-	const handleChange = (e) => {
-		setInputText(e.target.value);
-	};
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		const users = await searchUsers(inputText);
+    const users = await searchUsers(inputText);
 
-		dispatch({ type: 'GET_USERS', payload: users });
+    dispatch({ type: "GET_USERS", payload: users });
 
-		navigate(`/user/search`);
+    navigate(`/user/search`);
 
-		setInputText('');
-	};
+    setInputText("");
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<FormControl w={{ sm: '70vw', md: '100%' }} my={{ base: '20px' }}>
-				<InputGroup color='gray.400' size='lg'>
-					<InputLeftElement
-						pointerEvents='none'
-						children={<RiSearchLine size='22' />}
-					/>
-					<Input
-						placeholder='Search...'
-						id='search'
-						type='text'
-						value={inputText}
-						onChange={handleChange}
-						variant='flushed'
-					/>
-				</InputGroup>
-			</FormControl>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl w={{ sm: "100%" }} my={{ base: "20px" }}>
+        <InputGroup color="gray.400" size="lg">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<RiSearchLine size="22" />}
+          />
+          <Input
+            placeholder="Search..."
+            id="search"
+            type="text"
+            value={inputText}
+            onChange={handleChange}
+            variant="flushed"
+          />
+        </InputGroup>
+      </FormControl>
+    </form>
+  );
 };
 
 export default SearchUser;
