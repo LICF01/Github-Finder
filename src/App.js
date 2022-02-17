70d716c9fc572e77ee199264e9f2eea2132c0d96
LICF01 <lucascubilla@gmail.com>
2022-02-17 02:15:12 -0300

    Added the home route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './layout/Navbar';
-import { GithubProvider } from './context/GithubContext';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./layout/Navbar";
+import { GithubProvider } from "./context/GithubContext";
 
-import { Box } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/react";
 
-import UserResults from './components/user/UserResults';
-import User from './pages/User';
+import Home from "./pages/Home";
+import UserResults from "./components/user/UserResults";
+import User from "./pages/User";
 
 function App() {
-	return (
-		<GithubProvider>
-			<Box className='App' h='100%'>
-				<Navbar />
-				<Routes>
-					<Route path='/user/:login' element={<User />} />
-					<Route path='/user/search' element={<UserResults />} />
-				</Routes>
-			</Box>
-		</GithubProvider>
-	);
+  return (
+    <GithubProvider>
+      <Box h={"100%"}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/:login" element={<User />} />
+          <Route path="/user/search" element={<UserResults />} />
+        </Routes>
+      </Box>
+    </GithubProvider>
+  );
 }
 
 export default App;
