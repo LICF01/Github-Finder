434724c6197b1924d442e78f436f0ac3fb7727e9
LICF01 <lucascubilla@gmail.com>
2022-02-20 07:15:17 -0300

    Changed close button size and colors

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  Box,
   Drawer,
   DrawerOverlay,
   DrawerContent,
@@ -10,7 +9,7 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 
-import { GrClose } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
 
 import SearchUser from "./user/SearchUser";
 
@@ -20,21 +19,26 @@ export default function Cover({ onClose, isOpen }) {
   };
 
   const bgColor = useColorModeValue("white", "gray.900");
-  const color = useColorModeValue("white", "white");
+  const color = useColorModeValue("gray.900", "white");
 
   return (
     <>
       <Drawer onClose={onClose} isOpen={isOpen} size={"full"}>
         <DrawerOverlay />
-        <DrawerContent>
+        <DrawerContent bgColor={bgColor}>
           <DrawerHeader display={"flex"} justifyContent="end">
             <Icon
-              as={GrClose}
-              w={5}
-              h={5}
+              as={AiOutlineClose}
+              w={10}
+              h={10}
               mr={2}
               onClick={handleClose}
               color={color}
+              transition="all ease 0.5s"
+              _hover={{
+                cursor: "pointer",
+                color: "red.300",
+              }}
             />
           </DrawerHeader>
           <DrawerBody
@@ -42,6 +46,7 @@ export default function Cover({ onClose, isOpen }) {
             justifyContent="center"
             alignItems={"center"}
             h="100%"
+            mb={"200px"}
           >
             <SearchUser closeCover={handleClose} />
           </DrawerBody>
